eca7df67f65db498d0e015a7526c66af568ffafc
Gedridze <edvinas.kilciauskas@gmail.com>
2022-05-15 13:41:46 +0300

    remove module declaration

--- index.d.ts
+++ index.d.ts
@@ -2,8 +2,4 @@ import Vue, { Component, ComponentOptions } from 'vue';
 
 declare const VuePictureSwipe: Component & ComponentOptions<Vue>;
 
-declare module 'vue3-pictore-swipe' {
-    
-}
-
 export default VuePictureSwipe;
