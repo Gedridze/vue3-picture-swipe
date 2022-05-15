06ecbaa5ae1f262ae41bd3322ea6ec98130b78b7
Gedridze <edvinas.kilciauskas@gmail.com>
2022-05-15 13:47:32 +0300

    add module declaration to prevent ts build errors

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,6 @@
 import Vue, { Component, ComponentOptions } from 'vue';
 
 declare const VuePictureSwipe: Component & ComponentOptions<Vue>;
+declare module 'vue3-picture-swipe'
 
 export default VuePictureSwipe;
